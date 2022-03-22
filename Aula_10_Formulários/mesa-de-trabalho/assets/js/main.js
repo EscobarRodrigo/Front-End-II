146ca45abdcd77ef57731f9f1a7389037816b518
Rodrigo Escobar <88746440+EscobarRodrigo@users.noreply.github.com>
2022-03-22 08:50:23 -0300

    atualizando mesa10

--- Aula_10_Formulários/mesa-de-trabalho/assets/js/main.js
+++ Aula_10_Formulários/mesa-de-trabalho/assets/js/main.js
@@ -13,29 +13,31 @@ var inputUsuario;
 formulario.addEventListener("submit", function (evento) {
 
 inputUsuario = evento.target[0].value;
-    // console.log(inputUsuario);
+// console.log(inputUsuario);
 
-
-    // 2.1. Remova todos os números permitindo apenas texto
-    
-    var inputusuarioSemNúmeros = inputUsuario.replace(/[^a-zA-Z]+/g,"")
-    // console.log(inputusuarioSemEspacos);
+// 2.1. Remova todos os números permitindo apenas texto
+var inputUsuarioSemNúmeros = inputUsuario.replace(/[^a-zA-Z]+/g,"")
+// console.log(inputusuarioSemNúmeros);
 
 // 2.2. Remova todos os espaços vazios
-    var inputusuarioSemEspacos = inputUsuario.replace(/\s/g, "")
-    // console.log(inputusuarioSemEspacos);
+var inputusUarioSemEspacos = inputUsuario.replace(/\s/g, "")
+// console.log(inputusuarioSemEspacos);
 
-    alert(inputUsuario);
-    alert(inputusuarioSemEspacos);
-    alert(inputusuarioSemNúmeros); //(/[^a-zA-Z]+/g,"")
+alert(inputUsuario);
+alert(inputUsuarioSemEspacos);
+alert(inputUsuarioSemNúmeros); //(/[^a-zA-Z]+/g,"")
 
+// var listaDeCadastro = document.querySelector(".c-lista__item")
+// listaDeCadastro.innerText = inputUsuario;
 
 });
 
 // 3. Capture o evento de envio do formulário e disparar a rotina
 
 var listaDeCadastro = document.querySelector(".c-lista__item")
-listaDeCadastro.innerText = inputUsuario;
+listaDeCadastro.innerText = inputUsuarioSemNúmeros;
+campo.value = '';
+preventDefault();
 
 
 
@@ -44,15 +46,21 @@ listaDeCadastro.innerText = inputUsuario;
 
 //HUGO
 
-// // 1. Selecione o formulário
-// var formulario = document.getElementById('formularioDeCadastro');
+// var formulario = document.querySelector('#formularioDeCadastro');
 
-// // 2. Crie da rotina que será disparada a partir do evento de envio
-// var valorInput = document.querySelector('.c-form__campo');
-// formulario.addEventListener('submit', (event) => {
-//     console.log(valorInput.value);
+// function rotina(event){
 
-//     var valorInputDigitado = event.target[0].value;
+//     var valorInputDigitado = document.querySelector('.c-form__campo');
+//     var valorInputSemEspacos = valorInputDigitado.value.replace(/\s/g, '');
+//     var valorInputSemNumeros = valorInputSemEspacos.replace(/\d/g, '');
+    
+//     var listaCadastrados = document.querySelector('.c-lista__item'); 
+//     listaCadastrados.innerText = `Último item adicionado: ${valorInputSemNumeros}`;
+//     valorInputDigitado.value = '';
+//     event.preventDefault();
+// }
 
-//     console.log(valorInputDigitado);
-// })
+// document.addEventListener('submit', (event) => {
+//     rotina(event); 
+// });
+
